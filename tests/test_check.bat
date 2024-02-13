47b7a765be46780396531bf0f86af88cca04a25f
Juraj Tomori <juraj@dimensionstudio.co>
2024-02-13 16:36:57 +0000

    Use test assets in the test
    Allow i as a var name in code QA

--- tests/test_check.bat
+++ tests/test_check.bat
@@ -1,1 +1,9 @@
-rez env qa_assets dev_env -- qa check --check check_normalized_uvs --check check_polycount --check report_info --asset asset1 --asset asset2 --scene tests/tmp/check.hip
+rez env qa_assets dev_env -- qa check ^
+        --check check_normalized_uvs ^
+        --check check_polycount ^
+        --check asset_info ^
+        --asset tests/tmp/test_geo/1.obj ^
+        --asset tests/tmp/test_geo/2.bgeo.sc ^
+        --asset tests/tmp/test_geo/3.bgeo.sc ^
+        --asset tests/tmp/test_geo/4.fbx ^
+        --scene tests/tmp/check.hip
