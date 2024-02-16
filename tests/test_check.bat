0915cefc7baedb00aafbd96d29ab813d0d806eb8
Juraj Tomori <juraj@dimensionstudio.co>
2024-02-16 11:46:06 +0000

    Add json schema check into the test

--- tests/test_check.bat
+++ tests/test_check.bat
@@ -9,3 +9,12 @@ rez env qa_assets dev_env -- ^
         --asset tests/tmp/test_geo/4.fbx ^
         --asset tests/tmp/test_geo/missing ^
         --scene tests/tmp/check.hip
+
+rez env check_jsonschema -- ^
+    check-jsonschema ^
+        --schemafile qa_assets\data\schemas\report_v1.0.schema.json ^
+        tests\tmp\test_geo\reports\1.obj.json ^
+        tests\tmp\test_geo\reports\2.bgeo.sc.json ^
+        tests\tmp\test_geo\reports\3.bgeo.sc.json ^
+        tests\tmp\test_geo\reports\4.fbx.json ^
+        tests\tmp\test_geo\reports\missing.json
