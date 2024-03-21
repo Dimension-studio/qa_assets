aba19248fde7c8b62c351bb4ad917bd1b1703d8d
Juraj Tomori <juraj@dimensionstudio.co>
2024-03-21 10:31:54 +0000

    Add a demo cmd

---
+++ tests/demo.bat
@@ -0,0 +1,8 @@
+qa run ^
+    --pipeline_file tests\test_pipeline.json ^
+    --scene tests\tmp\run.hip ^
+    --asset tests\tmp\test_geo_run\1.obj ^
+    --asset tests\tmp\test_geo_run\2.bgeo.sc ^
+    --asset tests\tmp\test_geo_run\3.bgeo.sc ^
+    --asset tests\tmp\test_geo_run\4.fbx ^
+    --asset tests\tmp\test_geo_run\missing
