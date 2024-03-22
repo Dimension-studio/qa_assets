1a3cfd80263e36f23d7e3c6dd4119527689858ce
Juraj Tomori <juraj@dimensionstudio.co>
2024-03-22 09:51:38 +0000

    Add support for drag&drop of multiple files

--- web_viewer/main.js
+++ web_viewer/main.js
@@ -3,7 +3,7 @@
 // Function for setting drag & drop
 // https://stackoverflow.com/a/30100596
 function dropJSON(targetEl, callback) {
-    // disable default drag & drop functionality
+    // Disable the default drag & drop functionality
     targetEl.addEventListener("dragenter", function (e) {
         // Make the box border dashed
         targetEl.classList.add("border-dashed", "border-2")
@@ -11,24 +11,35 @@ function dropJSON(targetEl, callback) {
         e.preventDefault();
     });
 
+    // Prevent file from being opened by the browser
     targetEl.addEventListener("dragover", function (e) { e.preventDefault(); });
 
-    targetEl.addEventListener("drop", function (event) {
+    targetEl.addEventListener("drop", function (e) {
+        let files = e.dataTransfer.files;
 
-        let reader = new FileReader();
-        reader.onloadend = function () {
-            let data = JSON.parse(this.result);
+        // Iterate over all files
+        for (let i = 0; i < files.length; i++) {
+            // Create a new FileReader object with the onloadend handling
+            let reader = new FileReader();
 
-            // Call our passed JSON-processing function
-            callback(data);
-        };
+            reader.onloadend = function () {
+                let data = JSON.parse(this.result);
+                // Call our passed JSON-processing function
+                // Pass also file name
+                callback({
+                    name: files[i].name,
+                    data: data
+                });
+            };
 
-        reader.readAsText(event.dataTransfer.files[0]);
+            // Read the current file
+            reader.readAsText(files[i]);
+        }
 
         // Revert the dashed box border
         targetEl.classList.remove("border-dashed", "border-2")
 
-        event.preventDefault();
+        e.preventDefault();
     });
 }
 
